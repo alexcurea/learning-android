0a28c6a8e0945e1464a3390b27724e9c9ff6d601
alexcurea <alex20_96@yahoo.com>
2019-07-23 17:34:04 +0300

    first push

--- app/src/main/java/com/summerschool/learningandroid/MainActivity.java
+++ app/src/main/java/com/summerschool/learningandroid/MainActivity.java
@@ -1,13 +1,44 @@
 package com.summerschool.learningandroid;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button pickDateBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pickDateBtn = findViewById(R.id.button_ff);
+
+        pickDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                DatePickerDialog.OnDateSetListener OnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Log.d("DatePiccker", year + " " + month + " " + dayOfMonth);
+                    }
+
+                };
+
+
+                new DatePickerDialog(MainActivity.this, OnDateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 }
